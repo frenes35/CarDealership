781a57ea48229f67128c68c7861ff16ebd0dd6fa
Student <enesyilmaz4525@gmail.com>
2024-10-30 11:10:56 -0400

    UserInterface Class is done.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,48 @@
 package com.pluralsight;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+    private Dealership dealership;
+
+    public UserInterface() {
+        init();
+    }
+
+    private void init() {
+        DealershipFileManager fileManager = new DealershipFileManager();
+        dealership = fileManager.getDealership();
+    }
+
+    public void display() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1 - List All Vehicles");
+            System.out.println("99 - Quit");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    processGetAllVehiclesRequest();
+                    break;
+                case 99:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
+
+    public void processGetAllVehiclesRequest() {
+        List<Vehicle> vehicles = dealership.getAllVehicles();
+        displayVehicles(vehicles);
+    }
+
+    private void displayVehicles(List<Vehicle> vehicles) {
+        for (Vehicle vehicle : vehicles) {
+            System.out.println("VIN: " + vehicle.getVin() + ", Make: " + vehicle.getMake() + ", Model: " + vehicle.getModel() + ", Price: " + vehicle.getPrice());
+        }
+    }
 }
